faf627fcd2d13bddf6c5968013e0015780142beb
Anthony <anthonyallams@gmail.com>
2022-10-22 20:29:35 +0100

    Added Valid Palindrome solution and tests

--- tests/easy/test_validpalindrome.py
+++ tests/easy/test_validpalindrome.py
@@ -1,14 +1,13 @@
 import pytest
-from easy.binarysearch import ispalindrome1, ispalindrome2, ispalindrome3
+from easy.validpalindrome import isPalindrome1, isPalindrome2
 
 
 #To get the nums
 def test_ispalindrome():
     s = "A man, a plan, a canal: Panama"
     y = "race a car"
-    assert ispalindrome1(s) == True
-    assert ispalindrome2(s) == True
-    assert ispalindrome3(s) == True
-    assert ispalindrome1(y) == False
-    assert ispalindrome2(y) == False
-    assert ispalindrome3(y) == False
+    assert isPalindrome1(s) == True
+    # breakpoint()
+    assert isPalindrome2(s) == True
+    assert isPalindrome1(y) == False
+    assert isPalindrome2(y) == False
