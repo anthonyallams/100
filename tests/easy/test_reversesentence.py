c7d4e80e633f30939fc0176eb5848cde495b5dbe
Anthony <anthonyallams@gmail.com>
2022-10-27 22:04:53 +0100

    Added reverse sentence and test

--- tests/easy/test_reversesentence.py
+++ tests/easy/test_reversesentence.py
@@ -1,12 +1,13 @@
 import pytest
-from easy.reverseword import reverse1, reverse2
+from easy.reversesentence import reverse1, reverse2
 
 
 #To get the nums
 def test_reverseword():
     sentence = 'This is the best'
-    sentence2 = 'Hello Sir,  what can I get you? '
+    sentence2 = 'Hello Sir, what can I get you?'
     assert reverse1(sentence) == 'best the is This'
+    # breakpoint()
     assert reverse2(sentence) == 'best the is This'
     assert reverse1(sentence2) == 'you? get I can what Sir, Hello'
     assert reverse2(sentence2) == 'you? get I can what Sir, Hello'
