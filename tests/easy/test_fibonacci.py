1c2bc59a49aeb904f7ca54b72323ad317c1d56d0
Anthony <anthonyallams@gmail.com>
2022-11-16 15:57:24 +0000

    Modified fibonacci solution and added test

---
+++ tests/easy/test_fibonacci.py
@@ -0,0 +1,14 @@
+import pytest
+from easy.fibonacci import fibonacci1,fibonacci2,fibonacci3
+
+def test_fibonacci():
+    n1,n2,n3 = 10,1,12
+    assert(fibonacci1(n1)) == 55
+    assert(fibonacci1(n2)) == 1
+    assert(fibonacci1(n3)) == 144
+    assert(fibonacci2(n1)) == 55
+    assert(fibonacci2(n2)) == 1
+    assert(fibonacci2(n3)) == 144
+    assert(fibonacci3(n1)) == 55
+    assert(fibonacci3(n2)) == 1
+    assert(fibonacci3(n3)) == 144
