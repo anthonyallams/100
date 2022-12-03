fde313d4968222d2bcdc4f54aeb0c94fcf29b781
Anthony <anthonyallams@gmail.com>
2022-12-03 17:03:45 +0000

    Added Encode and Decode string with tests

--- tests/medium/test_encodedecode.py
+++ tests/medium/test_encodedecode.py
@@ -1,11 +1,20 @@
 import pytest
 from medium.encodedecode import encode, decode
 
-def test_encodedecode():
+def test_encode():
     strs,strs1 = ["lint","code","love","you"], ["we", "say", ":", "yes"]
     encoded_str = encode(strs)
     encoded_str1 = encode(strs1)
+    # breakpoint()
+    assert(type(encoded_str)) == str
+    assert(type(encoded_str1)) == str
     assert(encoded_str) != ["lint","code","love","you"]
     assert(encoded_str1) != ["we", "say", ":", "yes"]
+
+
+def test_decode():
+    strs,strs1 = ["lint","code","love","you"], ["we", "say", ":", "yes"]
+    encoded_str = encode(strs)
+    encoded_str1 = encode(strs1)
     assert(decode(encoded_str)) == ["lint","code","love","you"]
     assert(decode(encoded_str1)) == ["we", "say", ":", "yes"]
