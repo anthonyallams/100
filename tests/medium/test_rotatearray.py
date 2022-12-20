12fd79f46f1401eeb0f02a159edf7e65fcf0d606
Anthony <anthonyallams@gmail.com>
2022-12-20 13:33:16 +0000

    Added Rotate Array and tests

---
+++ tests/medium/test_rotatearray.py
@@ -0,0 +1,11 @@
+import pytest
+from medium.rotatearray import rotate,rotate1
+
+
+def test_rotatearray():
+    nums,k = [1,2,3,4,5,6,7],3 #[5,6,7,1,2,3,4]
+    nums1,k1 = [-1,-100,3,99],2#[3,99,-1,-100]
+    assert rotate(nums, k) == [5,6,7,1,2,3,4]
+    assert rotate(nums1, k1) == [3,99,-1,-100]
+    assert rotate1([1,2,3,4,5,6,7],k) == [5,6,7,1,2,3,4]
+    assert rotate1([-1,-100,3,99], k1) == [3,99,-1,-100]
