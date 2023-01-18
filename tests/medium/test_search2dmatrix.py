3a24d195cfa0a915f3ae5e7cde67cc014105f637
Anthony <anthonyallams@gmail.com>
2023-01-18 11:00:16 +0000

    Added Search 2d matrix and test

---
+++ tests/medium/test_search2dmatrix.py
@@ -0,0 +1,11 @@
+import pytest
+from medium.search2dmatrix import searchMatrix
+
+def test_search2dmatrix():
+    matrix,target = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3
+    assert(searchMatrix(matrix,target )) == True
+
+
+def test_search2dmatrix1():
+    matrix,target = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13
+    assert(searchMatrix(matrix,target)) == False
